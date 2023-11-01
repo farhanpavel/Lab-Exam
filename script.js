467bb8a6f496f138ea79df1142347cb71d59bcda
farhanpavel <farhanpavel3@gmail.com>
2023-11-01 11:10:54 +0600

    Done

---
+++ script.js
@@ -0,0 +1,16 @@
+// Assuming you have an image URL you want to insert
+const imageUrl = './images/Group1.png';
+
+(function() {
+    const button = document.querySelector('.shesh .btn');
+
+    button.addEventListener('click', function() {
+        // Create a new image element
+        const imgElement = document.createElement('img');
+        imgElement.src = imageUrl;
+        imgElement.alt = 'Description of the image'; // Add alt text for accessibility
+
+        // Append the image element to the body or any specific element on your page
+        document.body.appendChild(imgElement); // You can change document.body to any other parent element where you want to append the image
+    });
+})();
